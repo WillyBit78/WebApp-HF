41059989ea6525b0a49fcdd10ffc302b020dd4d0
WillyBit78 <willybit78@gmail.com>
2026-07-26 02:12:34 -0300

    fix: import useEffect in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AppProvider, useApp } from './context/AppContext';
 import { Navbar } from './components/Navbar';
 import { DashboardAdmin } from './components/DashboardAdmin';
